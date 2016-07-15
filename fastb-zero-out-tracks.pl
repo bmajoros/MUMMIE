1e8ce416c89474f743123b5be792a0c5d57b4fc3
Bill Majoros <martiandna@gmail.com>
2016-07-15 13:40:23 -0400

    update

--- fastb-zero-out-tracks.pl
+++ fastb-zero-out-tracks.pl
@@ -4,18 +4,40 @@ use ProgramName;
 use Fastb;
 
 my $name=ProgramName::get();
-die "$name <in.fastb> <track,name,list> <out.fastb>\n" unless @ARGV==3;
+die "$name <in.fastb> <track,name,list> <out.fastb>
+    in.fastb and out.fastb can instead be directory names
+" unless @ARGV==3;
 my ($infile,$trackList,$outfile)=@ARGV;
 
-my @tracks=split/,/,$trackList;
-my $fastb=new Fastb($infile);
-foreach my $trackName (@tracks) {
-  my $track=$fastb->getTrackByName($trackName);
-  my $L=$track->getLength();
-  my $data=$track->getData();
-  for(my $i=0 ; $i<$L ; ++$i) { $data->[$i]=0 }
+if($infile=~/\.fastb$/) { zeroOut($infile,$outfile) }
+else { # directory
+  my $indir=$infile; my $outdir=$outfile;
+  my @files=`ls $indir`;
+  my $n=@files;
+  for(my $i=0 ; $i<$n ; ++$i) {
+    my $file=$files[$i]; chomp $file;
+    if(/\/(\S+\.fastb$)/) { $file=$1 }
+    zeroOut("$indir/$file","$outdir/$file");
+  }
 }
-$fastb->save($outfile);
+
+sub zeroOut
+{
+  my ($infile,$outfile)=@_;
+
+  print "$infile => $outfile\n"; return;
+
+  my @tracks=split/,/,$trackList;
+  my $fastb=new Fastb($infile);
+  foreach my $trackName (@tracks) {
+    my $track=$fastb->getTrackByName($trackName);
+    my $L=$track->getLength();
+    my $data=$track->getData();
+    for(my $i=0 ; $i<$L ; ++$i) { $data->[$i]=0 }
+  }
+  $fastb->save($outfile);
+}
+
 
 
 
